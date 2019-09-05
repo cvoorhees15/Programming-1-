ea3917d7100043ee68a6d0f2ffa12e591a6dca50
c.voorhees <c.voorhees@10.242.0.129>
2019-09-05 19:38:27 -0400

    ready for review

---
+++ Voorhees Hello There/src/hellothere.java
@@ -0,0 +1,28 @@
+
+public class hellothere {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Project name: Hello There
+		//Name: Caleb Voorhees
+		
+		System.out.println("I am making this so Mr. Messner can get to know me a little bit better!\n");
+		System.out.println("\tMy name is Caleb.\n");
+		System.out.println("\tI love playing video games.\n");
+		System.out.println("\tMy favorite sport is lacrosse.\n");
+		System.out.println("\tMy favorite brand is Champion.\n");
+		System.out.println("\tI want to meet Joe Rogan.\n");
+		System.out.println("\tI love minecraft.\n");
+		System.out.println("\tI love wearing slippers.\n");
+		System.out.println("\tI like podcasts while driving.\n");
+		System.out.println("\tI want to develop video games or fun app games some day.\n");
+		System.out.println("\tMy girlfriends name is Shelby.\n");
+		System.out.println("Now I am practicing " + "using the plus sign");	
+		System.out.println("\n\tFirst = " + (5 * 6) + ",");
+	
+		
+		
+	}
+
+}
