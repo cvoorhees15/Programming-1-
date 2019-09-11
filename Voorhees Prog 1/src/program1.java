a1d07ca2a42a9ffdbdbff7d7ad8ccf529483ff2a
c.voorhees <c.voorhees@10.242.0.129>
2019-09-11 12:55:21 -0400

    ready

---
+++ Voorhees Prog 1/src/program1.java
@@ -0,0 +1,29 @@
+
+public class program1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Project: Program 1 
+		// Github: cvoorhees15 
+		// Author: Caleb Voorhees
+		// Description: Prints sentences using variables I have assigned.
+		// Version: 1.0
+		// Last Update: 11Sep2019
+		
+		// Birthday code.
+		int age = 19;
+		String myBirthday = "September 2nd";
+		String myFavoriteSport = "lacrosse";
+		System.out.print("My birthday is on " + myBirthday + "." + "\nOn my birthday I turned " + age + "." + "\nMy favorite sport is still " + myFavoriteSport + "." );
+		
+		// Code separated because each block is for a different topic.
+		
+		// Buffalo Bills score code.
+		double billsScore = 17;
+		char win = 'W';
+		String myFavoriteTeam = "Buffalo Bills";
+		System.out.println("\n\nMy favorite NFL team is the " + myFavoriteTeam + "." + "\nThis week they scored " + billsScore + " points." + "\nThey didn't play great, but it was enough to get the " + win + ".");
+		
+	}
+
+}
